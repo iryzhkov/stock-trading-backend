3d5d7bcc3622efd04890534366b85b36b5177579
Igor Ryzhkov <igor.o.ryzhkov@gmail.com>
2021-01-28 16:53:22 -0600

    Remove simulation data

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -6,6 +6,5 @@ from src.data.generated_stock_data import GeneratedStockData
 from src.data.randomized_stock_data import RandomizedStockData
 from src.data.real_stock_data import RealStockData
 from src.data.running_average_analysis import RunningAverageAnalysis
-from src.data.simulation_data import SimulationData
 from src.data.stock_data import StockData
 from src.data.stock_data_analysis import StockDataAnalysis
