4e8212fad4f78f1e1f3a47e3a5a83fd681eda656
Igor Ryzhkov <igor.o.ryzhkov@gmail.com>
2021-01-31 15:58:35 -0600

    Implement generated stock data

--- src/data/generated_stock_data.py
+++ src/data/generated_stock_data.py
@@ -1,5 +1,12 @@
 """Class for storing generated stock data.
 """
+import math
+
+from datetime import datetime
+
+import numpy as np
+import pandas as pd
+
 from src.data.stock_data import StockData
 
 
@@ -8,6 +15,19 @@ class GeneratedStockData(StockData):
     """Class for storing generated stock data.
     """
     name = "generated_stock_data"
+
+    def __init__(self, dependencies=None, evaluation_functions=None):
+        """Initializer for Generated Stock Data class
+
+        Args:
+            dependencies: a list of dependency ids for the data.
+
+        """
+        super(GeneratedStockData, self).__init__(dependencies)
+        if not evaluation_functions:
+            raise ValueError("No evaluation functions provided.")
+        self.evaluation_functions = evaluation_functions
+        self.anchor_date = datetime(2010, 1, 1)
 
     def prepare_data(self, from_date, to_date, stock_names, dependencies):
         """Data preparation.
@@ -20,4 +40,17 @@ class GeneratedStockData(StockData):
             stock_names: a list of stock names to prepare.
             dependencies: a list of prepared data dependencies.
         """
+        anchor_date = datetime(2010, 1, 1)
+        num_days = (to_date - from_date).days + 1
+        diff = from_date - anchor_date if from_date > anchor_date else anchor_date - from_date
+        diff = diff.days
+        array = np.zeros((num_days, len(stock_names)))
+        for day_index in range(num_days):
+            for stock_index in range(len(stock_names)):
+                evaluation_function_index = stock_index % len(self.evaluation_functions)
+                evaluation_function = self.evaluation_functions[evaluation_function_index]
+                array[day_index][stock_index] = eval(evaluation_function)
+            diff += 1
+        index = pd.Index(pd.date_range(from_date, to_date))
+        self.data = pd.DataFrame(array, index=index, columns=stock_names)
         self.ready = True
--- tests/data/test_generated_stock_data.py
+++ tests/data/test_generated_stock_data.py
@@ -1,5 +1,7 @@
 """Unit tests for generated stock data.
 """
+from datetime import datetime
+
 import unittest
 
 from src.data import GeneratedStockData
@@ -8,18 +10,33 @@ from src.data import GeneratedStockData
 class TestGeneratedStockData(unittest.TestCase):
     """Unit tests for generated stock data.
     """
+    def test_requires_evaluation_functions(self):
+        """Tests if raises exception when no evaluation functions are provided.
+        """
+        with self.assertRaises(ValueError):
+            _ = GeneratedStockData()
+
     def test_prepare_data(self):
         """Tests if the data is prepared properly.
         """
-        data = GeneratedStockData()
-        data.prepare_data(None, None, None, None)
+        from_date = datetime(2016, 1, 1)
+        to_date = datetime(2016, 2, 1)
+        stock_names = ["STOCK_1", "STOCK_2"]
+        data = GeneratedStockData(evaluation_functions=["100", "200"])
+        data.prepare_data(from_date, to_date, stock_names, [])
         self.assertTrue(data.ready)
+        self.assertTrue((stock_names == data.data.columns.tolist()))
+        self.assertEqual(100, data[from_date][0])
+        self.assertEqual(200, data[from_date][1])
 
     def test_resets_data(self):
         """Tests if the data is reset properly.
         """
-        data = GeneratedStockData()
-        data.prepare_data(None, None, None, None)
+        from_date = datetime(2016, 1, 1)
+        to_date = datetime(2016, 2, 1)
+        stock_names = ["STOCK_1", "STOCK_2"]
+        data = GeneratedStockData(evaluation_functions=["100"])
+        data.prepare_data(from_date, to_date, stock_names, [])
         self.assertTrue(data.ready)
         data.reset([])
         self.assertTrue(data.ready)
