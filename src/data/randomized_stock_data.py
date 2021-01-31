30f0c28191529b3dea558d9260e83a190899a2ce
Igor Ryzhkov <igor.o.ryzhkov@gmail.com>
2021-01-31 16:25:30 -0600

    Implement randomized stock data.

--- src/data/randomized_stock_data.py
+++ src/data/randomized_stock_data.py
@@ -1,23 +1,28 @@
 """Class for storing randomized stock data.
 """
+import numpy as np
+
 from src.data.stock_data import StockData
 
 
-# pylint: disable=too-few-public-methods
 class RandomizedStockData(StockData):
     """Class for storing randomized stock data.
     """
     name = "randomized_stock_data"
     expected_num_dependencies = 1
 
-    def __init__(self, dependencies=None):
+    def __init__(self, dependencies=None, mean=0, stdev=0.025):
         """Initializer for Randomized Stock Data class
 
         Args:
             dependencies: a list of dependency ids for the data.
+            mean: the mean relative difference for the randomized data.
+            stdev: the standart deviation for the relative difference.
         """
         super(RandomizedStockData, self).__init__(dependencies)
         self.id_str = "randomized_{}".format(self.dependencies[0])
+        self.mean = mean
+        self.stdev = stdev
 
     def prepare_data(self, from_date, to_date, stock_names, dependencies):
         """Data preparation.
@@ -31,6 +36,9 @@ class RandomizedStockData(StockData):
             dependencies: a list of prepared data dependencies.
         """
         self.ready = True
+        other_data = dependencies[0].data
+        noise = 1 + np.random.normal(self.mean, self.stdev, [len(other_data), len(stock_names)])
+        self.data = other_data.copy() * noise
 
     def reset(self, dependencies):
         """Reset for randomized stock data.
--- tests/data/test_randomized_stock_data.py
+++ tests/data/test_randomized_stock_data.py
@@ -1,8 +1,11 @@
 """Unit tests for randomized data.
 """
-import unittest
+from datetime import datetime
 
-from src.data import RandomizedStockData
+import unittest
+import numpy as np
+
+from src.data import RandomizedStockData, GeneratedStockData
 
 
 class TestRandomizedStockData(unittest.TestCase):
@@ -17,15 +20,41 @@ class TestRandomizedStockData(unittest.TestCase):
     def test_prepare_data(self):
         """Tests if the data is prepared properly.
         """
-        data = RandomizedStockData(dependencies=["stock_data"])
-        data.prepare_data(None, None, None, None)
+        from_date = datetime(2016, 1, 1)
+        to_date = datetime(2020, 1, 1)
+        stock_names = ["STOCK_1"]
+        value = 100
+        mean = 0.05
+        stdev = 0.025
+        dependency = GeneratedStockData(evaluation_functions=[str(value)])
+        data = RandomizedStockData(dependencies=["stock_data"], mean=mean, stdev=stdev)
+        dependency.prepare_data(from_date, to_date, stock_names, [])
+        data.prepare_data(from_date, to_date, stock_names, [dependency])
         self.assertTrue(data.ready)
+        self.assertTrue((stock_names == data.data.columns.tolist()))
+        self.assertTrue(np.isclose(value * (1 + mean), data.data["STOCK_1"].mean(), rtol=0.05))
+        self.assertTrue(np.isclose(value * stdev, data.data["STOCK_1"].std(), rtol=0.05))
 
     def test_resets_data(self):
         """Tests if the data is reset properly.
         """
-        data = RandomizedStockData(dependencies=["stock_data"])
-        data.prepare_data(None, None, None, None)
+        from_date = datetime(2016, 1, 1)
+        to_date = datetime(2016, 3, 1)
+        stock_names = ["STOCK_1"]
+        value = 100
+        mean = 0.05
+        stdev = 0.025
+        dependency = GeneratedStockData(evaluation_functions=[str(value)])
+        data = RandomizedStockData(dependencies=["stock_data"], mean=mean, stdev=stdev)
+        dependency.prepare_data(from_date, to_date, stock_names, [])
+        data.prepare_data(from_date, to_date, stock_names, [dependency])
+
+        data_before_reset = data.data["STOCK_1"].copy()
+
         self.assertTrue(data.ready)
         data.reset([True])
         self.assertFalse(data.ready)
+
+        data.prepare_data(from_date, to_date, stock_names, [dependency])
+        data_after_reset = data.data["STOCK_1"]
+        self.assertFalse(data_before_reset.equals(data_after_reset))
