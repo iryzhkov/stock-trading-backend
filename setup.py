b31aeff78d3866d95cddf030f3d8449568b1ab84
Igor Ryzhkov <igor.o.ryzhkov@gmail.com>
2021-01-31 23:43:36 -0600

    update classifiers in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="stock-trading-backend", # Replace with your own username
+    name="stock-trading-backend",
     version="0.0.1",
     author="Igor Ryzhkov",
     author_email="igor.o.ryzhkov@gmail.com",
@@ -14,9 +14,21 @@ setuptools.setup(
     url="https://github.com/iryzhkov/stock-trading-backend",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Environment :: Web Environment",
+        "Framework :: Flask",
+        "Framework :: Pytest",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
         "License :: MIT License",
+        "Natural Language :: English",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Office/Business :: Financial :: Investment",
+        "Topic :: Office/Business :: Financial",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
     python_requires='>=3.7',
 )
