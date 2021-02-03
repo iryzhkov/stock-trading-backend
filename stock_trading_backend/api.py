ded60e2b477dc0867667cbab2005a0f3941bf28a
Igor Ryzhkov <igor.o.ryzhkov@gmail.com>
2021-02-02 23:21:22 -0600

    Move backtest logic to its own file.

--- stock_trading_backend/api.py
+++ stock_trading_backend/api.py
@@ -1,12 +1,12 @@
 """APIs for the package.
 """
-from copy import deepcopy
 from datetime import datetime, timedelta
 import os
 
 from stock_trading_backend.agent import create_agent
-from stock_trading_backend.util import read_config_file
+from stock_trading_backend.backtest import backtest_agent
 from stock_trading_backend.simulation import StockMarketSimulation
+from stock_trading_backend.util import read_config_file
 
 ROOT = os.path.dirname(os.path.dirname(__file__))
 CONFIG_PATH = os.path.join(ROOT, "config")
@@ -66,35 +66,3 @@ def get_agent_object(agent_name="following_feature_agent_1", data_collection_nam
     reward_config = read_config_file(os.path.join("reward", reward_name + ".yaml"))
     agent = create_agent(agent_config, data_collection_config, reward_config)
     return agent
-
-# pylint: disable=too-many-arguments
-def backtest_agent(agent, from_date=None, to_date=None, start_balance=1000, commission=0,
-                   max_stock_owned=1):
-    """Backtests an agent with provided params.
-
-    Args:
-        from_date: datetime date for the start of the range.
-        to_date: datetime date for the end of the range.
-        start_balance: the starting balance.
-        commission: relative commission for each transcation.
-        max_stock_owned: a maximum number of different stocks that can be owned.
-    """
-    if from_date is None or to_date is None:
-        today = datetime.today()
-        today = datetime(today.year, today.month, today.day)
-        from_date = today - timedelta(days=60)
-        to_date = today - timedelta(days=1)
-
-    data_collection_config = deepcopy(agent.data_collection_config)
-    reward_config = deepcopy(agent.reward_config)
-
-    simulation = StockMarketSimulation(data_collection_config, from_date=from_date,
-                                       to_date=to_date, min_start_balance=start_balance,
-                                       max_start_balance=start_balance, commission=commission,
-                                       max_stock_owned=max_stock_owned, reward_config=reward_config)
-
-    observation = simulation.reset()
-    while not simulation.done:
-        action = agent.make_decision(observation, simulation)
-        observation, _, _ = simulation.step(action)
-    return simulation.overall_reward
